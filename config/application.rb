66592fed7a74a3d33643a32af2c0edb7fa5822eb
adeanlika <adeanlika01@gmail.com>
2017-04-24 17:59:42 +0700

    fix groupdate timezone

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,7 @@ Bundler.require(*Rails.groups)
 module APISmarthome
   class Application < Rails::Application
     config.time_zone = "Bangkok"
+    Groupdate.time_zone = "Bangkok"
     config.active_record.default_timezone = :local
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
