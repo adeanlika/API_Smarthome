26a77eb74c95a8d70738d3ecdcb0827b92f6dbe4
adeanlika <adeanlika01@gmail.com>
2017-04-30 12:51:21 +0700

    Fix Migration 8

--- db/migrate/20170217102240_drop_reference_home_user_to_user.rb
+++
@@ -1,5 +0,0 @@
-class DropReferenceHomeUserToUser < ActiveRecord::Migration[5.0]
-  def change
-    remove_reference :homes_users, :user
-  end
-end
--- db/migrate/20170308045736_rename_light_alert_logs_to_co2_alert_logs.rb
+++
@@ -1,5 +0,0 @@
-class RenameLightAlertLogsToCo2AlertLogs < ActiveRecord::Migration[5.0]
-  def change
-  	rename_table :light_alert_logs, :CO2_alert_logs
-  end
-end
