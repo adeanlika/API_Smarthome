3c9366edb51afb2ec5cb71eacba2d0a2cf2fe9bf
adeanlika <adeanlika01@gmail.com>
2017-04-30 12:55:03 +0700

    Fix Migration 9

--- db/migrate/20170406120244_add_device_to_light.rb
+++
@@ -1,5 +0,0 @@
-class AddDeviceToLight < ActiveRecord::Migration[5.0]
-  def change
-    add_reference :lights, :devices, foreign_key: true
-  end
-end
--- db/migrate/20170410053929_drop_device_id_from_light.rb
+++
@@ -1,5 +0,0 @@
-class DropDeviceIdFromLight < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :lights, :devices_id
-  end
-end
