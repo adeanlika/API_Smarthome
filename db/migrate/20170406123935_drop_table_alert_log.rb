c21fd230ffe31de5dd7e3de2d12bdc5b8dbe0a40
adeanlika <adeanlika01@gmail.com>
2017-04-30 12:56:21 +0700

    Fix Migration 10

--- db/migrate/20170406123935_drop_table_alert_log.rb
+++
@@ -1,9 +0,0 @@
-class DropTableAlertLog < ActiveRecord::Migration[5.0]
-  def change
-    drop_table :motion_alert_logs
-    drop_table :hum_alert_logs
-    drop_table :temp_alert_logs
-    drop_table :co_alert_logs
-    drop_table :energy_alert_logs
-  end
-end
