36b1553d273c99b8aa7971938c7da7a15cf04b63
adeanlika <adeanlika01@gmail.com>
2017-06-04 13:10:36 +0700

    add fiture show current alert

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -50,30 +50,48 @@ class AlertsController < ApiController
   end
   def current_notif
       @home = Home.find(params[:home_id])
+      @current_notif = []
       if @home.upperenergy_flag == true
-        @energy = Alert.where(["alert_type = ?", "Energy"]).order('created_at ASC').last
-      elsif @home.cost_limit_flag == true
-        @cost = Alert.where(["alert_type = ?", "Cost"]).order('created_at ASC').last
+        @energy = Alert.where(["alert_type = ?", "Energy"]).order('created_at ASC').last.to_json
+        @current_notif << @energy
       end
-      # @current_notif = @energy.as_json.merge(@cost.as_json)
-      render json: @cost
-  #     elsif @home.uppertemp_flag == true
-  #       @uppertemp = Alert.where('alert_type = Temperature' && 'status = Temperature too high').order('created_at ASC').last
-  #     elsif @home.lowertemp_flag == true
-  #
-  #     elsif @home.upperhum_flag == true
-  #       @upperhum = Alert.where('alert_type = Humidity' && 'status = Humidity too high').order('created_at ASC').last
-  #     elsif @home.lowerhum_flag == true
-  #
-  #     elsif @home.upperco_flag == true
-  # @uppertemp = Alert.where('alert_type = Carbondioxide' && 'status = Carbondioxide too high').order('created_at ASC').last
-  #     elsif @home.lowerco_flag == true
-  #
-  #     elsif @home.upperflux_flag == true
-  #       @uppertemp = Alert.where('alert_type = Light' && 'status = Light too high').order('created_at ASC').last
-  #     elsif @home.lowerflux_flag == true
-
-
+      if @home.cost_limit_flag == true
+        @cost = Alert.where(["alert_type = ?", "Cost"]).order('created_at ASC').last.to_json
+        @current_notif << @cost
+      end
+      if @home.uppertemp_flag == true
+         @uppertemp = Alert.where(:alert_type => "Temperature",:status => "Temperature too high").order('created_at ASC').last.to_json
+         @current_notif << @uppertemp
+      end
+      if @home.lowertemp_flag == true
+          @lowertemp = Alert.where(:alert_type => "Temperature",:status => "Temperature too low").order('created_at ASC').last.to_json
+          @current_notif << @lowertemp
+      end
+      if @home.upperhum_flag == true
+         @upperhum = Alert.where('alert_type = Humidity' && 'status = Humidity too high').order('created_at ASC').last.to_json
+         @current_notif << @upperhum
+      end
+      if @home.lowerhum_flag == true
+         @lowerhum = Alert.where('alert_type = Humidity' && 'status = Humidity too low').order('created_at ASC').last.to_json
+         @current_notif << @lowerhum
+      end
+      if @home.upperco_flag == true
+         @upperco = Alert.where('alert_type = Carbondioxide' && 'status = Carbondioxide too high').order('created_at ASC').last.to_json
+         @current_notif << @upperco
+      end
+      if @home.lowerco_flag == true
+         @lowerco = Alert.where('alert_type = Carbondioxide' && 'status = Carbondioxide too low').order('created_at ASC').last.to_json
+         @current_notif << @lowerco
+      end
+      if @home.upperflux_flag == true
+         @upperflux = Alert.where('alert_type = Light' && 'status = Light too high').order('created_at ASC').last.to_json
+         @current_notif << @upperflux
+      end
+      if @home.lowerflux_flag == true
+         @lowerflux = Alert.where('alert_type = Light' && 'status = Light too low').order('created_at ASC').last.to_json
+         @current_notif << @lowerflux
+      end
+    render json: @current_notif.map{ |s| JSON[s] }.sort_by{|i| -i['created_at'].to_time.to_i}#sort_by { |hsh| hsh['created_at'] }
 
   end
   private
